2d0357d6c6d9936632c425cbb66032762e31df91
tokojkis <alexander.csore@gmail.com>
2024-09-30 14:47:58 +0200

    added more to state and render

--- public/app.js
+++ public/app.js
@@ -1,21 +1,30 @@
 //state = all den data som appen har lagrat utifrån vad användaren gör
 //state innehåller också vår config eller "start-värden"
 
-state = {
+const state = {
   shop: [
-    { vegetable: "broccoli", amount: 12, color: "green" },
-    { vegetable: "potato", amount: 25, color: "brown" },
-    { vegetable: "tomato", amount: 8 },
+    { vegetable: "🥦broccoli", amount: 12, color: "green" },
+    { vegetable: "🥔potato", amount: 25, color: "brown" },
+    { vegetable: "🍅tomato", amount: 8 },
   ],
+  addVegetable: function (vegetable, amount) {
+    this.shop.push({ vegetable, amount });
+  },
 };
 
-for (const v of state.shop) {
-  //skapa ny divtagg
-  const vegetable = document.createElement("div");
-  //lägg till text i divtaggen
-  vegetable.innerHTML = `${v.vegetable}: ${v.amount} ${
-    v.color === undefined ? "" : "," + v.color
-  }`;
-  //lägger till taggen i domen
-  document.querySelector(".shop").appendChild(vegetable);
-}
+//render = visa upp nya "GUI"
+const renderShop = () => {
+  //tömma shop-div först!
+  for (const v of state.shop) {
+    //skapa ny divtagg
+    const vegetable = document.createElement("div");
+    //lägg till text i divtaggen
+    vegetable.innerHTML = `${v.vegetable}: ${v.amount} ${
+      v.color === undefined ? "" : "," + v.color
+    }`;
+    //lägger till taggen i domen
+    document.querySelector(".shop").appendChild(vegetable);
+  }
+};
+state.addVegetable("cucumber", 25);
+renderShop();
